a2830b332beae345f73e6a81890ebc3cb4bc2b4f
KimLHill <kimberley@midwayelectronics.co.uk>
2021-09-24 21:14:13 +0000

    Add updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
@@ -7,6 +9,19 @@ def bag_contents(request):
     # set total and product count to 0 to start count from
     total = 0
     product_count = 0
+    # Get bag from session if one or create one
+    bag = request.session.get('bag', {})
+
+    # Add up total cost and product count of shopping bag contents
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
