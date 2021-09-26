02b00d5d0e0d15c06116494d9186316928f609a9
KimLHill <kimberley@midwayelectronics.co.uk>
2021-09-26 12:43:26 +0000

    Add core payment functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,9 +6,9 @@
 */
 
 /* Get stripe public key and slice off quotations from ends */
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -42,4 +42,39 @@ card.addEventListener('change', function (event) {
     } else {
         errorDiv.textContent = '';
     }
+});
+
+/* Handle form submit */
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    /* Prevent multiple submissions */
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    /* Send card details to Stripe */
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        /* If card fails show error message to user */
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            /* Allow user to fix error in card details */
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        /* If card passes, submit form */
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
