554e6931623da3c44cb00679fab1eaebd567b804
KimLHill <kimberley@midwayelectronics.co.uk>
2021-09-26 11:12:41 +0000

    Add Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JdjpeGCKuMmEVHDHYDMD7kixVSmSja1JFHlRTXum8uk3D4UWqSw7fYEB1Ejivz8ricUz8VvA7tqk0pP7jbWRezg00sUHRBisL',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
