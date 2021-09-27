ab15f5f3548dfdd317f86d2618fd933f933ff404
KimLHill <kimberley@midwayelectronics.co.uk>
2021-09-27 16:25:15 +0000

    Add blog models and admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Blog
 
 # Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'author',
+        'description',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+admin.site.register(Blog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Blog model. Requires name, author and description. 
+class Blog(models.Model):
+    name = models.CharField(max_length=254)
+    author = models.CharField(max_length=254)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
